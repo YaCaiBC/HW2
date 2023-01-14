75ffa98f428c4a1860e6d50e9b234e23e5e1c1e2
aecameron <aec.cameron@gmail.com>
2023-01-13 19:40:30 -0800

    Alesia first draft HW2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HW2
 2nd HW for Prog 209
+
+Ya Cai & Alesia Cameron
--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
 
-        document.getElementById("textbox1").value = "success";
-    });
-});
+    document.getElementById("button").addEventListener("click", function () {
+        //parsing int
+        let numOne = parseInt(document.getElementById('textbox1').value);
+        let numTwo = parseInt(document.getElementById('textbox2').value);
+        let numThree = parseInt(document.getElementById('textbox3').value);
+
+        //adding numbers
+        let answer = (numOne + numTwo + numThree) / 3;
+
+        //display
+        document.getElementById('output').innerHTML = answer;
+
+        //document.getElementById("textbox1").value = "success";
+    });//function
+}); // DOMContentLoaded
 
